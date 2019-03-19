f935d9065b42c4b2f43699e4db044b6ff3a0ee20
PriyankaHanumansetti <priyanka2k9539@gmail.com>
2019-03-19 14:56:14 +0530

    Update AppConfiguration.java

--- src/main/java/com/sample/webhook/AppConfiguration.java
+++ src/main/java/com/sample/webhook/AppConfiguration.java
@@ -1,19 +1,8 @@
 package com.sample.webhook;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.cache.CacheBuilderSpec;
-import com.oracle.ci.api.authentication.dropwizard.ApiAuthenticatorConfiguration;
 import io.dropwizard.Configuration;
-import io.dropwizard.db.DataSourceFactory;
-import io.dropwizard.db.ManagedDataSource;
-import io.dropwizard.setup.Environment;
-
-import javax.inject.Provider;
-import javax.sql.DataSource;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.util.Map;
 
 public class AppConfiguration extends Configuration {
 
